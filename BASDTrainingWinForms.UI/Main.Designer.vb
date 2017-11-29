57db3cc9fdf1f7ab6fe518681300168cbac36537
Rainier Bacareza <RB004259604@mercury.local>
2017-11-29 05:27:09 -0600

    added some modules

--- BASDTrainingWinForms.UI/Main.Designer.vb
+++ BASDTrainingWinForms.UI/Main.Designer.vb
@@ -22,16 +22,70 @@ Partial Class Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.threadingBackgroundWorker = New System.ComponentModel.BackgroundWorker()
+        Me.ThreadSampleOneButton = New System.Windows.Forms.Button()
+        Me.ThreadSampleTwoButton = New System.Windows.Forms.Button()
+        Me.ThreadingProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'threadingBackgroundWorker
+        '
+        '
+        'ThreadSampleOneButton
+        '
+        Me.ThreadSampleOneButton.Location = New System.Drawing.Point(136, 61)
+        Me.ThreadSampleOneButton.Name = "ThreadSampleOneButton"
+        Me.ThreadSampleOneButton.Size = New System.Drawing.Size(75, 23)
+        Me.ThreadSampleOneButton.TabIndex = 0
+        Me.ThreadSampleOneButton.Text = "Thread"
+        Me.ThreadSampleOneButton.UseVisualStyleBackColor = True
+        '
+        'ThreadSampleTwoButton
+        '
+        Me.ThreadSampleTwoButton.Location = New System.Drawing.Point(217, 61)
+        Me.ThreadSampleTwoButton.Name = "ThreadSampleTwoButton"
+        Me.ThreadSampleTwoButton.Size = New System.Drawing.Size(75, 23)
+        Me.ThreadSampleTwoButton.TabIndex = 1
+        Me.ThreadSampleTwoButton.Text = "No Thread"
+        Me.ThreadSampleTwoButton.UseVisualStyleBackColor = True
+        '
+        'ThreadingProgressBar
+        '
+        Me.ThreadingProgressBar.Location = New System.Drawing.Point(6, 19)
+        Me.ThreadingProgressBar.Name = "ThreadingProgressBar"
+        Me.ThreadingProgressBar.Size = New System.Drawing.Size(286, 23)
+        Me.ThreadingProgressBar.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ThreadingProgressBar)
+        Me.GroupBox1.Controls.Add(Me.ThreadSampleOneButton)
+        Me.GroupBox1.Controls.Add(Me.ThreadSampleTwoButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(363, 178)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(303, 94)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Threading"
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(683, 517)
+        Me.ClientSize = New System.Drawing.Size(678, 283)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Main"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents threadingBackgroundWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ThreadSampleOneButton As System.Windows.Forms.Button
+    Friend WithEvents ThreadSampleTwoButton As System.Windows.Forms.Button
+    Friend WithEvents ThreadingProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
 End Class
--- BASDTrainingWinForms.UI/Main.vb
+++ BASDTrainingWinForms.UI/Main.vb
@@ -4,4 +4,61 @@
 
     End Sub
 
+    Private Sub ThreadSampleOneButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ThreadSampleOneButton.Click
+        Me.ThreadSampleOneButton.Enabled = False
+        Me.threadingBackgroundWorker.WorkerReportsProgress = True
+        Me.threadingBackgroundWorker.RunWorkerAsync()
+
+    End Sub
+
+    Private Sub threadingBackgroundWorker_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles threadingBackgroundWorker.DoWork
+        Try
+
+            Dim counter As Integer
+            counter = 0
+            Do
+                counter += 1
+                Me.threadingBackgroundWorker.ReportProgress(counter)
+                System.Threading.Thread.Sleep(200)
+            Loop While counter < 100
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+
+    End Sub
+
+    Private Sub threadingBackgroundWorker_RunWorkerCompleted(ByVal sender As System.Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles threadingBackgroundWorker.RunWorkerCompleted
+        Try
+            MessageBox.Show("Threading Complete.")
+            Me.ThreadSampleOneButton.Enabled = True
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub threadingBackgroundWorker_ProgressChanged(ByVal sender As System.Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles threadingBackgroundWorker.ProgressChanged
+        Try
+            Me.ThreadingProgressBar.Value = e.ProgressPercentage
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ThreadSampleTwoButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ThreadSampleTwoButton.Click
+        Try
+
+            Dim counter As Integer
+            counter = 0
+            Do
+                counter += 1
+                Me.ThreadingProgressBar.Value = counter
+                System.Threading.Thread.Sleep(200)
+            Loop While counter < 100
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        
+    End Sub
+
 End Class
